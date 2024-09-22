a316d754efdec5f839181312432d575a54333100
Adarsh Priya kashyap <120103822+Adarshkashyap2002@users.noreply.github.com>
2024-09-22 13:20:39 +0000

    #

---
+++ 01_Basic/02_DataTypes.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// alert(3 + 3)
+console.log(3 + 3)
+console.log("Adarsh")
+
+
+
+let name = ("Adarsh")
+let age =20
+let isLoggin = false
+let state
+
+console.log(typeof null)
---
+++ 01_Basic/03_conversionoperation.js
@@ -0,0 +1,7 @@
+let score ="40"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueofnumber = Number(score)
+console.log(typeof valueofnumber)
